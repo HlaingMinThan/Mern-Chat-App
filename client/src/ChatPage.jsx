238954586fb9ecd56490fea4437eb84e5dc86f05
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-03-08 23:06:41 +0630

    update styling

--- client/src/ChatPage.jsx
+++ client/src/ChatPage.jsx
@@ -40,9 +40,9 @@ export default function ChatPage() {
 
     return (
         <div className="flex h-screen">
-            <div className="bg-white w-1/3 flex flex-col">
+            <div className="bg-white w-1/4 flex flex-col">
                 <div className="flex-grow ">
-                    <h2 className="font-bold text-2xl p-3 text-blue-600 flex gap-2 items-center my-3 justify-center">
+                    <h2 className="font-bold text-3xl p-3 text-blue-600 flex gap-2 items-center my-3  ml-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
                         </svg>
@@ -51,7 +51,7 @@ export default function ChatPage() {
                     {!!onlinePeople.length && onlinePeople.map(username => (
                         <div key={username} className="border border-b-1 p-3 text-lg flex gap-4 items-center">
                             <Avatar username={username} />
-                            <span className="text-xl">{username}</span>
+                            <span className="text-lg">{username}</span>
                         </div>
                     ))}
                 </div>
@@ -67,7 +67,7 @@ export default function ChatPage() {
                         className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm">logout</button>
                 </div>
             </div>
-            <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+            <div className="flex flex-col bg-blue-50 w-3/4 p-2">
                 <div className="flex-grow">
 
                     <div className="relative h-full">
