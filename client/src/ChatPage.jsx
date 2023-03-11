faff00721d9ef13a4a84786b412e29dd8d4ce7c8
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-03-11 13:37:25 +0630

    ui improvement

--- client/src/ChatPage.jsx
+++ client/src/ChatPage.jsx
@@ -128,7 +128,7 @@ export default function ChatPage() {
                         )
                     ))}
                 </div>
-                <div className="p-2 text-center flex items-center justify-center">
+                <div className="p-2 text-center flex items-center justify-between">
                     <span className="mr-2 text-sm text-gray-600 flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                             <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
@@ -137,7 +137,7 @@ export default function ChatPage() {
                     </span>
                     <button
                         onClick={logout}
-                        className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm">logout</button>
+                        className="text-sm bg-red-600 py-1 px-2 text-white border rounded-sm">Logout</button>
                 </div>
             </div>
             <div className="flex flex-col bg-blue-50 w-3/4">
